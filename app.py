af33e705f476b0802c08de58a585b87af6b03c63
evulet1 <elvirochkata@gmail.com>
2021-10-28 15:42:02 +0300

    'packing'

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
-
-def greet():
-    print(f'Hello, {user_name.get() or "World"}')
 
 root = tk.Tk()
-root.title('Greeter')
 
-user_name= tk.StringVar()
+tk.Label(root, text='Label left', bg='green').pack(
+    side='left', expand=True, fill='both'
+)
 
-name_label = ttk.Label(root, text='Name: ')
-name_label.pack(side='left', padx=(0, 10))
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
-name_entry.pack(side='left')
-name_entry.focus()
+tk.Label(root, text='Label1', bg='green').pack(side='left', fill='both', expand=True)
+tk.Label(root, text='Label2', bg='red').pack(side='top', fill='both', expand=True)
 
-greet_button = ttk.Button(root, text='Greetings!', command = greet)
-greet_button.pack(side='left', fill='x', expand=True)
+
+tk.Label(root, text='Label left', bg='green').pack(
+    side='left', expand=True, fill='both'
+)
+
+tk.Label(root, text='Label left', bg='green').pack(
+    side='left', expand=True, fill='both'
+)
 
 
 root.mainloop()
