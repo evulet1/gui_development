2cbc8abaa79f949fffddddab2bb7f2f764f59e3f
evulet1 <elvirochkata@gmail.com>
2021-10-29 10:06:21 +0300

    'frames'

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 
-tk.Label(root, text='Label left', bg='green').pack(
-    side='left', expand=True, fill='both'
-)
+main = ttk.Frame(root)
+main.pack(side='left', fill='both', expand=True)
 
-tk.Label(root, text='Label1', bg='green').pack(side='left', fill='both', expand=True)
-tk.Label(root, text='Label2', bg='red').pack(side='top', fill='both', expand=True)
-
+tk.Label(main, text='Label1', bg='red').pack(side='top', fill='both', expand=True)
+tk.Label(main, text='Label2', bg='red').pack(side='top', fill='both', expand=True)
 
 tk.Label(root, text='Label left', bg='green').pack(
     side='left', expand=True, fill='both'
 )
-
-tk.Label(root, text='Label left', bg='green').pack(
-    side='left', expand=True, fill='both'
-)
-
 
 root.mainloop()
