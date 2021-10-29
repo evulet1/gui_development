4f00a19aaf5405286112e0b6ccc34452c9ff0e9a
evulet1 <elvirochkata@gmail.com>
2021-10-29 16:14:38 +0300

    'menu'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 
-def cerate_file():
+def create_file():
     text_area = tk.Text(notebook)
     text_area.pack(fill='both', expand=True)
     notebook.add(text_area, text='Untitled')
@@ -10,13 +10,24 @@ def cerate_file():
 
 
 root = tk.Tk()
+root.title('Text Editor')
+root.option_add('*tearOff', False)
 
 main = ttk.Frame(root)
 main.pack(fill='both', expand=True, padx=1, pady=(4, 0))
 
+menubar = tk.Menu()
+root.config(menu=menubar)
+
+file_menu = tk.Menu(menubar)
+menubar.add_cascade(menu=file_menu, label='File')
+
+
+file_menu.add_command(label='New', command=create_file)
+
 notebook = ttk.Notebook(main)
 notebook.pack(fill='both', expand=True)
 
-cerate_file()
+create_file()
 
 root.mainloop()
