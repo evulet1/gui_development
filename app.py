fd9a5ac8083dd4da6bcfd52dc13d4dc449a1ca05
evulet1 <elvirochkata@gmail.com>
2021-11-10 22:29:49 +0300

    saving_files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 
 def create_file():
@@ -7,6 +8,22 @@ def create_file():
     text_area.pack(fill='both', expand=True)
     notebook.add(text_area, text='Untitled')
     notebook.select(text_area)
+
+def save_file():
+    file_path = filedialog.asksaveasfilename()
+
+    try:
+        filename = os.path.basename(file_path)
+        text_widget = root.nametowidget(notebook.select())
+        content = text_widget.get_content("1.0","end-1c")
+
+        with open(file_path, "w") as file:
+            file.write(content)
+    except (AttributeError, FileNotFoundError):
+        print("Save operation cancelled")
+        return
+
+    notebook.tab("current", text = filename)
 
 
 root = tk.Tk()
@@ -24,6 +41,7 @@ menubar.add_cascade(menu=file_menu, label='File')
 
 
 file_menu.add_command(label='New', command=create_file)
+file_menu.add_command(label='Save', command=save_file)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill='both', expand=True)
